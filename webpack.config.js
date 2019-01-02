412c104ce616ede196419757a46a6f30acc40035
Friedemann Bartels <mail@friedemannbartels.de>
2019-01-02 22:13:52 +0100

    fix: link minified bundle in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,12 @@ const path = require('path')
 
 const config = {
   entry: {
-    'demo': './src/demo'
+    'demo': './src/demo',
+    'prevent-pointer-focus': './src/index'
   },
 
   output: {
-    filename: 'prevent-pointer-focus.min.js',
+    filename: '[name].min.js',
     path: path.join(__dirname, 'dist'),
     publicPath: '/',
     libraryTarget: 'umd'
@@ -27,7 +28,8 @@ const config = {
   plugins: [
     new MinifyPlugin(),
     new HtmlWebpackPlugin({
-      template: 'src/demo.html'
+      template: 'src/demo.html',
+      excludeChunks: ['prevent-pointer-focus']
     })
   ]
 }
