33a94f01b5709f4fc50029294ebbe7b3029c6dff
Friedemann Bartels <mail@friedemannbartels.de>
2019-01-05 18:33:46 +0100

    feat: handle focus position

--- src/blurHelper.js
+++ src/blurHelper.js
@@ -1,5 +1,23 @@
 import addOnetimeEventListeners from './addOnetimeEventListeners'
 
+const focusNext = (element) => {
+  const focusElement = element.parentNode.nextElementSibling
+  const focus = () => {
+    focusElement.removeEventListener('focus', focus)
+    focusElement.classList.add('pressed')
+    focusElement.addEventListener('blur', blur)
+  }
+  const blur = () => {
+    focusElement.removeEventListener('blur', blur)
+    focusElement.classList.remove('pressed')
+    focusElement.removeAttribute('tabindex')
+  }
+
+  focusElement.setAttribute('tabindex', 0)
+  focusElement.addEventListener('focus', focus)
+  focusElement.focus()
+}
+
 export const blurOnChange = (element) => {
   element.classList.add('pressed')
 
@@ -10,6 +28,7 @@ export const blurOnChange = (element) => {
     ['change', element, () => {
       element.classList.remove('pressed')
       element.blur()
+      focusNext(element)
     }]
   ])
 }
@@ -31,6 +50,7 @@ export const blurOnMouseup = (element) => {
     ['mouseup', element, () => {
       element.classList.remove('pressed')
       element.blur()
+      focusNext(element)
     }]
   ])
 }
@@ -38,7 +58,10 @@ export const blurOnMouseup = (element) => {
 export const blurOnFocus = (element, label) => {
   if (label) {
     addOnetimeEventListeners([
-      ['focus', element, () => element.blur()],
+      ['focus', element, () => {
+        element.blur()
+        focusNext(element)
+      }],
       ['mouseup', document, event => event.target !== label],
       ['keydown', document]
     ])
@@ -49,6 +72,7 @@ export const blurOnFocus = (element, label) => {
   if (element === document.activeElement) {
     setTimeout(() => {
       element.blur()
+      focusNext(element)
     }, 1)
 
     return
@@ -57,6 +81,10 @@ export const blurOnFocus = (element, label) => {
   addOnetimeEventListeners([
     ['focus', element, () => {
       element.blur()
+      focusNext(element)
+    }],
+    ['click', element, () => {
+      focusNext(element)
     }]
   ])
 }
